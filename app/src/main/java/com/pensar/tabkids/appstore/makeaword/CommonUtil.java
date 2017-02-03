6fc9005141b5ec32b07abe6ead9b9a3eaae68460
vibhanshu007 <vibhanshumail849@gmail.com>
2017-02-03 13:05:52 +0530

    Update...Layout..

--- app/src/main/java/com/pensar/tabkids/appstore/makeaword/CommonUtil.java
+++ app/src/main/java/com/pensar/tabkids/appstore/makeaword/CommonUtil.java
@@ -1,6 +1,14 @@
 package com.pensar.tabkids.appstore.makeaword;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by root on 2/2/17.
@@ -15,5 +23,25 @@ public class CommonUtil {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    public static Bitmap getDataFromAsserts(Context context, String filePath){
+        AssetManager assetManager = context.getAssets();
+
+        Bitmap bitmap =null;
+        InputStream inputStream;
+
+        try {
+            inputStream = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(inputStream);
+        }catch (IOException e){
+
+        }
+        return bitmap;
+    }
+
+    public static BitmapDrawable getBitmapDrawableFromAsserts(Context context, String filepath){
+        return new BitmapDrawable(context.getResources(),getDataFromAsserts(context,filepath));
+
     }
 }
